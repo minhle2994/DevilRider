5286f7aca6b611e163f720e5d7adb4e92f234513
minhld_570 <minhle2994@gmail.com>
2014-03-15 09:30:45 +0700

    Main menu

---
+++ Assets/Script/LoadLogoStudio.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLogoStudio : MonoBehaviour {
+	private Texture studioLogo;
+
+	// Use this for initialization
+	void Start () {
+		studioLogo = Resources.Load ("LogoStudio") as Texture;
+	}
+	
+	// Update is called once per frame
+	void OnGUI() {
+		GUI.DrawTexture (new Rect(0, 0, Screen.width, Screen.height), studioLogo);
+		Application.LoadLevel (0);
+	}
+
+	
+	void Update () {
+		
+	}
+}
