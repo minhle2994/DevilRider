6513e099f7901060b99b52fbc3f6875ddd02f19a
khoalevan <khoalevan1994@gmail.com>
2014-03-15 09:23:22 +0700

    add animation

--- Assets/Script/CountingScore.cs
+++ Assets/Script/CountingScore.cs
@@ -5,7 +5,7 @@ public class CountingScore : MonoBehaviour {
 	public GUIText ScoreLabel;
 	public int Score = 0;
 	int[] isCounted = new int[10];
-    public GameObject aim, player;
+	public GameObject aim, DevilRider;
 	// Use this for initialization
 	void Start () {
         aim.transform.renderer.enabled = false;
@@ -28,7 +28,7 @@ public class CountingScore : MonoBehaviour {
 				isCounted[i] = 0;
 			}			 
 
-            float dist = child.position.z - player.transform.position.z;
+			float dist = child.position.z - DevilRider.transform.position.z;
             if (dist > 0 && (nearestDist < 0 || nearestDist > dist)) {
                 nearestObj = i;
                 nearestDist = dist;
@@ -40,10 +40,10 @@ public class CountingScore : MonoBehaviour {
 
 		if (nearestDist > 2 && nearestDist < 15 && PlayerPrefs.GetInt ("canShoot") == 1) {
             Vector3 tmp = transform.GetChild(nearestObj).position;
-            aim.transform.position = new Vector3(player.transform.position.x, tmp.y, tmp.z - 1);
+			aim.transform.position = new Vector3(DevilRider.transform.position.x, tmp.y, tmp.z - 1);
             aim.transform.renderer.enabled = true;
            
-            if (Mathf.Abs(player.transform.position.x - tmp.x) < 0.5) {
+			if (Mathf.Abs(DevilRider.transform.position.x - tmp.x) < 0.5) {
                 Vector3 np = transform.GetChild(nearestObj).position;
                 np.x = 0;
                 np += new Vector3(Random.Range(-4.5f, 4.5f), 0, Random.Range(58, 60));
--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -11,123 +11,112 @@ public class PlayerControl : MonoBehaviour {
 	public int CoinNum = 0;
 	public AudioClip collectCoinSound;
 	private Vector3 moveDirection = Vector3.zero;
+	public Animator devilRiderAnimator;
+	public float currentTime = 0;
+	public GameObject nitroItem;
+	public NitroControl nitroControl;
 
 	//public bool canShoot = false;
     void Start () {
+		Time.timeScale = 1;
         PlayerPrefs.SetInt("canShoot", 0);
-		//this.GetComponent<Rigidbody> ().AddForce (Vector3.down*10);
-        //transform.GetChild(0).renderer.enabled = false;
+		devilRiderAnimator = GetComponent<Animator> ();
+		devilRiderAnimator.SetBool ("Dead", false);
+		nitroItem = GameObject.Find("Nitro");
+		nitroControl = nitroItem.GetComponent<NitroControl> ();
     }
     
-    void Update () {
-		MovingSpeed += Time.deltaTime / 5;
-        // Detect xem đang chạy trên mobile hay trên các thiết bị khác
-        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
-        {
-            AccelerometerDirection = Input.acceleration;   
-        }
-        else
-        {
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                AccelerometerDirection.x = AccelerometerSensitivity - 1;
-            }
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                AccelerometerDirection.x = -AccelerometerSensitivity + 1;
-            }
-            if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
-            {
-                AccelerometerDirection.x = 0.0f;
-            }
-        }
+	void Update () {
+	}
 
-        // Di chuyển xe thẳng hướng phía trước
-        //transform.Translate(new Vector3(0, 0, MovingSpeed * Time.deltaTime));
+	void FixedUpdate(){
+		detectPlatform ();
+		if(MovingSpeed < 35) MovingSpeed += Time.deltaTime / 6;
+		movementManagement ();
+	}
 
+	// Detect the platform which is running
+	void detectPlatform(){
+		if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer) {
+						AccelerometerDirection = Input.acceleration;   
+		} else {
+				if (Input.GetKey (KeyCode.LeftArrow)) {
+						AccelerometerDirection.x = AccelerometerSensitivity - 1;
+				}
+				if (Input.GetKey (KeyCode.RightArrow)) {
+						AccelerometerDirection.x = -AccelerometerSensitivity + 1;
+				}
+				if (Input.GetKeyUp (KeyCode.LeftArrow) || Input.GetKeyUp (KeyCode.RightArrow)) {
+						AccelerometerDirection.x = 0.0f;
+				}
+		}
+	}
+
+	void movementManagement(){
+		// Di chuyển xe thẳng hướng phía trước
+		//transform.Translate(new Vector3(0, 0, MovingSpeed * Time.deltaTime));
 		moveDirection = transform.TransformDirection (new Vector3 (0, 0, MovingSpeed));
-
 		this.GetComponent<CharacterController>().Move (moveDirection * Time.deltaTime);
-
+		
 		// Camera cũng phải chạy theo, giữ 1 khoảng cách nhất định với xe
-        Camera.main.transform.position = new Vector3(0, transform.position.y + 7, transform.position.z - 11);
-        
-        if (AccelerometerDirection.x > AccelerometerSensitivity)
-        {
-            // Khi nghiên phone thì cho xe quẹo trái
-            transform.Rotate (new Vector3 (0, 30 * Time.deltaTime, 0), Space.Self);
-        }
-        else if (AccelerometerDirection.x < -AccelerometerSensitivity)
-        {
-            // Quẹo phải
-            transform.Rotate (new Vector3 (0, -30 * Time.deltaTime, 0), Space.Self);
-        }
-        else
-        {
-            // Mặc định thì xoay xe hướng về phía trước nó sẽ chạy thẳng
-            transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0, 0, 0), 7 * Time.deltaTime);
-        }
-//
-//        // Di chuyển xe thẳng hướng phía trước
-//        //transform.Translate(new Vector3(0, 0, MovingSpeed * Time.deltaTime));
-//		this.GetComponent<CharacterController>().Move (transform.TransformDirection(new Vector3 (0, 0, MovingSpeed * Time.deltaTime)));
-//        // Camera cũng phải chạy theo, giữ 1 khoảng cách nhất định với xe
-//        Camera.main.transform.position = new Vector3(0, transform.position.y + 8, transform.position.z - 4);
-//        
-//		if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer) {
-//		    foreach (Touch touch in Input.touches) {
-//		        if ((btnLeft.HitTest (touch.position)) && (this.transform.position.x > -2.5)) {
-//					transform.Rotate (new Vector3 (0, -20 * Time.deltaTime, 0), Space.Self);
-//				} else
-//			    if ((btnRight.HitTest (touch.position)) && (this.transform.position.x < 2.5)) {
-//			        transform.Rotate (new Vector3 (0, 20 * Time.deltaTime, 0), Space.Self);
-//				} 
-//			}
-//		    if (Input.touchCount == 0)
-//		        transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0, 0, 0), 5 * Time.deltaTime);
-//		}
-//        else
-//        {
-//            if (Input.GetKey(KeyCode.LeftArrow))
-//            {
-//                transform.Rotate (new Vector3 (0, -20 * Time.deltaTime, 0), Space.Self);
-//            }else 
-//            if (Input.GetKey(KeyCode.RightArrow))
-//            {
-//                transform.Rotate (new Vector3 (0, 20 * Time.deltaTime, 0), Space.Self);
-//            }else 
-//                transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0, 0, 0), 5 * Time.deltaTime);
-//        }        
+		Camera.main.transform.position = new Vector3(0, transform.position.y + 7, transform.position.z - 10);
+		
+		if (AccelerometerDirection.x > AccelerometerSensitivity)
+		{
+			// Khi nghiên phone thì cho xe quẹo trái
+			transform.Rotate (new Vector3 (0, 30 * Time.deltaTime, 0), Space.Self);
+		}
+		else if (AccelerometerDirection.x < -AccelerometerSensitivity)
+		{
+			// Quẹo phải
+			transform.Rotate (new Vector3 (0, -30 * Time.deltaTime, 0), Space.Self);
+		}
+		else
+		{
+			// Mặc định thì xoay xe hướng về phía trước nó sẽ chạy thẳng
+			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0, 0, 0), 7 * Time.deltaTime);
+		}
+	}
+	
+
+	IEnumerator timeToGameOver(){
+		while (Time.realtimeSinceStartup - currentTime < 0.4)
+			yield return null;
+		Time.timeScale = 0;
+		while (Time.realtimeSinceStartup - currentTime < 4)
+			yield return null;
+		Application.LoadLevel(2);
 	}
 
     void OnTriggerEnter (Collider other){
 		if (other.name == "Car") {
-			other.renderer.enabled = false;
-			PlayerPrefs.SetInt("coins", PlayerPrefs.GetInt("coins") + CoinNum);
-			Application.LoadLevel(2);
+			if(nitroControl.nitroState == true || MovingSpeed > 40f){
+				Vector3 np = other.transform.position;
+				np.x = 0;
+				np += new Vector3(Random.Range(-4.5f, 4.5f), 0, Random.Range(90, 110));
+				other.transform.position = np;
+				CoinNum += 10;
+				CoinLabel.text = CoinLabel.text.Substring(0, 5) + CoinNum.ToString();
+			}else{
+				other.renderer.enabled = false;
+				devilRiderAnimator.SetBool("Dead", true);
+				currentTime = Time.realtimeSinceStartup;
+				PlayerPrefs.SetInt("coins", PlayerPrefs.GetInt("coins") + CoinNum);
+				
+				StartCoroutine(timeToGameOver());
+			}
 		}
 
 		if (other.name == "Coin") {
-            Vector3 np = other.transform.position;
-            np.x = 0;
-            np += new Vector3(Random.Range(-4.5f, 4.5f), 0, Random.Range(58, 60));
-            other.transform.position = np;
-
 			CoinNum++;
 			CoinLabel.text = CoinLabel.text.Substring(0, 5) + CoinNum.ToString();
 			audio.PlayOneShot(collectCoinSound, 1);
 		}
 
         if (other.name == "Gun") {
-            Vector3 np = other.transform.position;
-            np.x = 0;
-            np += new Vector3(Random.Range(-4.5f, 4.5f), 0, Random.Range(110, 130));
-            other.transform.position = np;
-
-            PlayerPrefs.SetInt("canShoot", 1);
+		    PlayerPrefs.SetInt("canShoot", 1);
         }
     }
-
-
+	
 }
  
