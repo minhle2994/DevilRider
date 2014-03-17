5ab740b2eb987c2a0103095a9b6836df7f956b61
minhld_570 <minhle2994@gmail.com>
2014-03-17 15:23:19 +0700

    fix position appear of nitro and gun

--- Assets/Script/NitroControl.cs
+++ Assets/Script/NitroControl.cs
@@ -32,7 +32,7 @@ public class NitroControl : MonoBehaviour {
 		if (Camera.main.transform.position.z >= (this.transform.position.z + 20)) {
 			Vector3 np = this.transform.position;
 			np.x = 0;
-			np += new Vector3(Random.Range(-4.5f, 4.5f), 0, Random.Range(58, 60));
+			np += new Vector3(Random.Range(-4.5f, 4.5f), 0, Random.Range(80, 90));
 			this.transform.position = np;
 			if (this.renderer.enabled == false)
 				this.renderer.enabled = true;
