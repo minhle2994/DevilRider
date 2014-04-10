90b34b9a93f15833f46520797911a395a530f5c8
minhld_570 <minhle2994@gmail.com>
2014-04-10 15:01:41 +0700

    change movement of motor

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -6,7 +6,7 @@ public class PlayerControl : MonoBehaviour {
 	public float jumpSpeed = 10.0f;
 	public float gravity = 20.0f;
     private Vector3 AccelerometerDirection;             // Trục cảm ứng nghiên
-    public float AccelerometerSensitivity = 0.05f;      // Độ nhạy cảm ứng nghiên
+    public float AccelerometerSensitivity = 0.1f;      // Độ nhạy cảm ứng nghiên
 	public GUIText CoinLabel;
 	public int CoinNum = 0;
 	public AudioClip collectCoinSound;
@@ -25,6 +25,8 @@ public class PlayerControl : MonoBehaviour {
 	private bool newHighScore = false;
 	public GameObject Nitro;
 	public Vector3 turnLeft, turnRight;
+	private float angle;
+
 
     void Start () {
 		Time.timeScale = 1;
@@ -37,6 +39,8 @@ public class PlayerControl : MonoBehaviour {
 		PlayerPrefs.SetInt ("Score", 0);
 		flag = false;
 		newHighScore = false;
+		turnRight = transform.TransformDirection (new Vector3 (10, 0, 0));
+		turnLeft = transform.TransformDirection (new Vector3 (-10, 0, 0));
   }
     
 	void Update () {
@@ -50,48 +54,69 @@ public class PlayerControl : MonoBehaviour {
 
 	// Detect the platform which is running
 	void detectPlatform(){
-		if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer) {
-						AccelerometerDirection = Input.acceleration;   
-		} else {
-				if (Input.GetKey (KeyCode.LeftArrow)) {
-						AccelerometerDirection.x = AccelerometerSensitivity - 1;
-				}
-				if (Input.GetKey (KeyCode.RightArrow)) {
-						AccelerometerDirection.x = -AccelerometerSensitivity + 1;
-				}
-				if (Input.GetKeyUp (KeyCode.LeftArrow) || Input.GetKeyUp (KeyCode.RightArrow)) {
-						AccelerometerDirection.x = 0.0f;
-				}
-		}
+//		if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer) {
+//						AccelerometerDirection = Input.acceleration;   
+//		} else {
+//				if (Input.GetKey (KeyCode.LeftArrow)) {
+//						AccelerometerDirection.x = AccelerometerSensitivity - 1;
+//				}
+//				else if (Input.GetKey (KeyCode.RightArrow)) {
+//						AccelerometerDirection.x = -AccelerometerSensitivity + 1;
+//				}
+//				else {
+//						AccelerometerDirection.x = 0.0f;
+//				}
+//		}
+		AccelerometerDirection = Input.acceleration;   
+		AccelerometerDirection.x = Mathf.Min (AccelerometerDirection.x, 0.7f);
 	}
 
 	void movementManagement(){
 		// Di chuyển xe thẳng hướng phía trước
 		//transform.Translate(new Vector3(0, 0, MovingSpeed * Time.deltaTime));
 		moveDirection = transform.TransformDirection (new Vector3 (0, 0, MovingSpeed));
-		turnLeft = transform.TransformDirection (new Vector3 (5, 0, 0));
-		turnRight = transform.TransformDirection (new Vector3 (-5, 0, 0));
 
+		//Debug.Log (transform.eulerAngles.z);
+		//Debug.Log (AccelerometerDirection.x);
 		this.GetComponent<CharacterController>().Move (moveDirection * Time.deltaTime);
-		Debug.Log (transform.eulerAngles.z);
 		// Camera cũng phải chạy theo, giữ 1 khoảng cách nhất định với xe
 		Camera.main.transform.position = new Vector3(transform.position.x , transform.position.y + 5, transform.position.z - 8);
 		Nitro.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
-		if (AccelerometerDirection.x > AccelerometerSensitivity)
+
+		if ( AccelerometerDirection.x > AccelerometerSensitivity)
 		{
-			if (transform.eulerAngles.z > 315 || Mathf.Abs(transform.eulerAngles.z) < 1
-			    || (transform.eulerAngles.z >= 0 && transform.eulerAngles.z <= 45)){
-				transform.Rotate (new Vector3 (0, 7 * Time.deltaTime, -30 * Time.deltaTime), Space.Self);
+			if (transform.eulerAngles.z > 330 || Mathf.Abs(transform.eulerAngles.z) < 1
+			    || (transform.eulerAngles.z >= 0 && transform.eulerAngles.z <= 32)){
+
+				if (transform.eulerAngles.z > 320 || Mathf.Abs(transform.eulerAngles.z) < 1)
+					angle = Mathf.Min(transform.eulerAngles.z, 360 - transform.eulerAngles.z);
+				else 
+					angle = 0;
+				
+				if (angle != 0)
+					transform.Rotate (new Vector3 (0, 0 * Time.deltaTime, -50 * Time.deltaTime * AccelerometerDirection.x * 2.0f), Space.Self);
+				else 
+					transform.Rotate (new Vector3 (0, 0 * Time.deltaTime, -100 * Time.deltaTime * AccelerometerDirection.x * 2.0f), Space.Self);
 			}
-			this.GetComponent<CharacterController>().Move (turnLeft * Time.deltaTime);
+			this.GetComponent<CharacterController>().Move (turnRight * Time.deltaTime * AccelerometerDirection.x * 2.0f);
 		}
 		else if (AccelerometerDirection.x < -AccelerometerSensitivity)
 		{
-			if (transform.eulerAngles.z < 45 || Mathf.Abs(transform.eulerAngles.z) < 1
-			    || (transform.eulerAngles.z <= 360 && transform.eulerAngles.z >= 315)){
-				transform.Rotate (new Vector3 (0, -7 * Time.deltaTime, 30 * Time.deltaTime), Space.Self);
+			Debug.Log("left");
+			if (transform.eulerAngles.z < 30 || Mathf.Abs(transform.eulerAngles.z) < 1
+			    || (transform.eulerAngles.z <= 360 && transform.eulerAngles.z >= 328)){
+
+				if (transform.eulerAngles.z < 30 || Mathf.Abs(transform.eulerAngles.z) < 1)
+					angle = transform.eulerAngles.z;
+				else 
+					angle = 0;
+
+				if (angle != 0)
+					transform.Rotate (new Vector3 (0, -0 * Time.deltaTime, 50 * Time.deltaTime * -AccelerometerDirection.x * 2.0f), Space.Self);
+				else 
+					transform.Rotate (new Vector3 (0, -0 * Time.deltaTime, 100 * Time.deltaTime -AccelerometerDirection.x * 2.0f), Space.Self);
 			}
-			this.GetComponent<CharacterController>().Move (turnRight * Time.deltaTime);
+			this.GetComponent<CharacterController>().Move (turnLeft * Time.deltaTime * -AccelerometerDirection.x * 2.0f);
 		}
 		else
 		{
